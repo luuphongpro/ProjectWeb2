c0014b21387c280242c935ff464234c8b3ec40f5
NQD <ngquangduong9a11@gmail.com>
2024-04-08 12:45:38 +0700

    (chưa kết hợp được chức năng hiện sản phẩm) chức năng tìm kiếm

--- pages/front/product.php
+++ pages/front/product.php
@@ -1,3 +1,32 @@
+            <div class="searchBar">
+                <form class="filter">
+                    <div class="item ten">
+                        <label>Tên</label>
+                        <input name="txtTimkiem" value="<?php if(isset($_GET['txtTimkiem'])){echo $_GET['txtTimkiem'];} ?>" type="text">
+                    </div>
+                    <div class="item submit">
+                        <input class="btnSearch" type="submit" name="timkiem" value="Tìm kiếm">
+                    </div>
+                    <div class="item">
+                        <label>Danh mục</label>
+                        <select name="category">
+                            <option value="">---</option>
+                            <option value="B">Hamburger</option>
+                            <option value="C">Gà</option>
+                            <option value="P">Pizza</option>
+                            <option value="D">Nước</option>
+                        </select>
+                    </div>
+                    <div class="item">
+                        <label>Tối thiểu</label>
+                        <input name="minPrice" type="number" min = 0 value = "<?php if(isset($_GET['minPrice'])){echo $_GET['minPrice'];}?>">
+                    </div>
+                    <div class="item">
+                        <label>Tối đa</label>
+                        <input name="maxPrice" type="number" min = 0 value = "<?php if(isset($_GET['maxPrice'])){echo $_GET['maxPrice'];} ?>">
+                    </div>
+                </form>
+            </div> 
 <?php
 // Kết nối đến cơ sở dữ liệu
 $servername = "localhost";
@@ -13,28 +42,30 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Truy vấn để lấy tổng số sản phẩm
-$perPage = 9;
 
-$sqltotal = "SELECT * FROM product";
-$tongsotrang = $conn->query($sqltotal);
-$leng = $tongsotrang->num_rows;
-$pageTotal = ceil($leng / $perPage);
-
-// Xác định trang hiện tại
-if(isset($_GET['trang'])){
-    $page = $_GET['trang'];
-} else {
-    $page = 1;
-}
-$begin = ($page - 1) * $perPage;
 
 // Truy vấn để lấy thông tin sản phẩm
-$sql = "SELECT * FROM product ORDER BY MaSP DESC LIMIT $begin , $perPage ";
+$sql = "SELECT * FROM product ORDER BY MaSP DESC";
 $result = $conn->query($sql);
 
 // Hiển thị sản phẩm nếu có
 if ($result->num_rows > 0) {
+    // Truy vấn để lấy tổng số sản phẩm
+$perPage = 9;
+
+    $sqltotal = "SELECT * FROM product";
+    $tongsotrang = $conn->query($sqltotal);
+    $leng = $tongsotrang->num_rows;
+    $pageTotal = ceil($leng / $perPage);
+
+    // Xác định trang hiện tại
+    if(isset($_GET['trang'])){
+        $page = $_GET['trang'];
+    } else {
+        $page = 1;
+    }
+    $begin = ($page - 1) * $perPage;
+    
     echo '<section class="food_section layout_padding-bottom">
             <div class="container">
               <div class="heading_container heading_center">
@@ -43,39 +74,10 @@ if ($result->num_rows > 0) {
                 </h2>
               </div>
 
-              <div class="searchBar">
-              <form class="filter">
-                  <div class="item ten">
-                      <label>Tên</label>
-                      <input name="txtTimkiem" type="text">
-                  </div>
-                  <div class="item submit">
-                      <input class="btnSearch" type="submit" name="timkiem" value="Tìm kiếm">
-                  </div>
-                  <div class="item">
-                      <label>Danh mục</label>
-                      <select name="category">
-                          <option value="">---</option>
-                          <option value="B">Hamburger</option>
-                          <option value="C">Gà</option>
-                          <option value="P">Pizza</option>
-                          <option value="D">Nước</option>
-                      </select>
-                  </div>
-                  <div class="item">
-                      <label>Tối thiểu</label>
-                      <input name="minPrice" type="number" min=0>
-                  </div>
-                  <div class="item">
-                      <label>Tối đa</label>
-                      <input name="maxPrice" type="number" min=0>
-                  </div>
-              </form>
-          </div> 
-
               <div class="row">';
      // Duyệt qua mỗi dòng dữ liệu
      $productIndex = 0;
+     /*
      while($row = $result->fetch_assoc()) {
          echo "<div class='col-sm-6 col-lg-4'>";
              echo "<div class='filters-content'>";
@@ -162,7 +164,117 @@ if ($result->num_rows > 0) {
  
          $productIndex++;
      }
- 
+    */
+
+    if(isset($_GET['timkiem'])){
+        $name = $_GET['txtTimkiem'];
+        $category = $_GET['category'];
+        
+        if(empty($_GET['minPrice'])){
+            $minPrice = 0;
+        }else{
+            $minPrice = $_GET['minPrice'];
+        }
+
+        if(empty($_GET['maxPrice'])){
+            $maxPrice = 999999999;
+        }else{
+            $maxPrice = $_GET['maxPrice'];
+        }
+
+        $filterdata = "SELECT * FROM `product` WHERE `TenSP` LIKE '%$name%' && `MaSP` LIKE '%$category%' && GiaSP BETWEEN $minPrice AND $maxPrice LIMIT $begin , $perPage";
+        $filterdata_run = mysqli_query($conn, $filterdata);
+        if (mysqli_num_rows($filterdata_run) > 0){
+            foreach($filterdata_run as $row) {
+                echo "<div class='col-sm-6 col-lg-4'>";
+                    echo "<div class='filters-content'>";
+                        echo "<div class='box'>";
+                            echo "<div class='detail-box'>";
+                                echo "<div class='img-box'>";
+                                    echo "<img src='./img/" . $row['IMG'] . "' alt='" . $row['TenSP'] . "' style='width: 100%; height: 100%;'>";
+                                echo "</div>";
+                                echo "<h2 style='margin-bottom: 20px; margin-top: 20px;'>" . $row['TenSP'] . "</h2>";
+                                echo "<div class='options'>";
+                                    echo "<p>Giá: " . number_format($row['GiaSP']) . " VNĐ</p>";
+                                    echo "<a href=''><i class='fa-solid fa-cart-shopping' style='color:#ffff'></i></a>";
+                                echo "</div>";
+                                echo "<p>Số lượng còn lại: " . $row['SoLuongSP'] . "</p>";
+                                echo "<button class='detail-button' data-product-index='" . $productIndex . "'>Chi tiết</button>";
+                            echo "</div>";
+                        echo "</div>";
+                    echo "</div>";
+                echo "</div>";
+
+                echo "<div class='overlay' data-product-index='" . $productIndex . "'>";
+                echo "<div class='info' data-product-index='" . $productIndex . "'>";
+                echo "<button type='button' class='close' onClick='closeProductInfo()'>+</button>";
+
+                echo "<div class='left'>";
+                echo '<h2 style="font-family: \'Roboto Mono\', monospace; margin-bottom: 25px; margin-left: 70px; margin-bottom:30px; ">Chi tiết sản phẩm</h2>';        
+                echo "<div class='img-box'>";
+                echo "<img id='imgbig' src='./img/" . $row['IMG'] . "' alt='" . $row['TenSP'] . "' >";
+                echo "</div>";
+                echo "<p id='ttsp'>" . $row['TTSP'] ."</p>";
+                echo "</div>";
+
+                echo "<div class='right'>";
+                echo "<h2 style='margin-bottom: 20px; margin-top: 20px; font-size: 2.5rem;'>" . $row['TenSP'] . "</h2>";
+                echo "<h4>Giá: " . number_format($row['GiaSP']) . " VNĐ</h4>";
+                //
+                echo "<div class='right-flex'>";
+                echo "<h4>Số lượng</h4>";
+
+                echo "<div>";
+                echo "<button class='quantitydown' onClick='quantitydown()'>-</button><input type='text' id='quantity' value='1'><button class='quantityup' onClick='quantityup()'>+</button>";
+                echo "</div>";
+
+                echo "</div>";
+                //
+                echo "<input type='hidden' name='idp' id='idp' value='1'>";
+                echo "<button class='addtocart' onclick=getquantity()>Thêm vào giỏ</button>";
+
+                echo '<div class="footer_social" style="font-size: 30px; margin-bottom: 15px; Color: #626071;">
+                    <a href="">
+                    <i class="fa fa-facebook" aria-hidden="true"></i>
+                    </a>
+                    <a href="">
+                    <i class="fa fa-twitter" aria-hidden="true"></i>
+                    </a>
+                    <a href="">
+                    <i class="fa fa-linkedin" aria-hidden="true"></i>
+                    </a>
+                    <a href="">
+                    <i class="fa fa-instagram" aria-hidden="true"></i>
+                    </a>
+                    <a href="">
+                    <i class="fa fa-pinterest" aria-hidden="true"></i>
+                    </a>
+                </div>';
+
+                echo "<div class='callphone'>";
+                echo "<p>Gọi mua hàng:";
+                echo "<a>1111.1111.111</a>";
+                echo "<span>(10h-10h)</span>";
+                echo "</p>";
+                echo "</div>";
+
+                echo "<div class='r-note'><i class='fa-solid fa-truck-fast fa-lg'></i><p>Giao hàng tận nơi</p></div>";
+                echo "<div class='r-note'><i class='fa-solid fa-hand-holding-dollar fa-lg'></i><p>Ưu đãi mỗi ngày</p></div>";
+                echo "<div class='r-note'><i class='fa-regular fa-credit-card fa-lg'></i></i><p>Thanh toán COD,BANK,MOMO</p></div>";
+
+                echo "</div>";
+                echo "</div>";
+                echo "</div>";
+
+                $productIndex++;
+                
+            }
+        }
+        else {
+            echo "Không tìm thấy kết quả";
+        }
+    }
+
      echo '</div></div></section>';
 
      echo '<div style="display: flex; justify-content: center; align-items: center;">';
