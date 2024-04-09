88896415f057e70fed34f784d469323086659b85
PhuHuynh <134149189+luuphongpro@users.noreply.github.com>
2024-04-09 22:47:03 +0700

    push giao diện admin

---
+++ JS/admin.js
@@ -0,0 +1,35 @@
+function cook() {
+    window.location = "index.php";
+}
+// Kiểm tra disabled bởi vì cần làm chức năng quản lý chức năng cho nhóm quyền
+function init(index){
+    var name;
+    //Đang phát triển với tham số truyền vào tương ứng với các quyền...
+    name=[[],["Quản lý sản phẩm","Thống kê doanh thu"],["Quản lý Bán Hàng","Thống kê doanh thu"]]
+    $(".container.disabled").removeClass("disabled");
+    $(".container").each(function(){
+        name[index].forEach((i)=>{
+            if($(this).find(".item-container").text().trim()==i){
+                console.log($(this).find(".item-container").text().trim())
+                $(this).find(".item-container").addClass("disabled");
+            }
+        })
+    })
+}
+init(1);
+$("#qlsanpham").click(function(){
+    if(!$(this).hasClass("disabled")){
+        $(".menudrop-qlsanpham").toggle("active")
+    }
+})
+$("#donhang").click(function(){
+    if(!$(this).hasClass("disabled")){
+        $(".menudrop-donhang").toggle("active")
+    }
+})
+$("#taikhoan").click(function(){
+    if(!$(this).hasClass("disabled")){
+        $(".menudrop-qltk").toggle("active")
+    }
+})
+//Khi show chức năng, phải render vào #right-content trách bị trùng giao diện giữa mọi người
---
+++ admin.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./fonts/themify-icons-font/themify-icons/themify-icons.css">
+    <link rel="stylesheet" href="./CSS/bootstrap.css">
+    <link rel="stylesheet" href="./CSS/admin.css">
+    <title>Document</title>
+</head>
+<body>
+    <div id="rapper-admin">
+        <div id="header">
+            <div class="container-item"><i class="ti-layout-grid2-alt"></i> Administrator</div>
+            <div class="container-item" onclick="cook()"><i class="ti-share-alt"></i> Vào trang web</div>
+            <div class="container-item right-item">Xin chào: admin <i class="ti-angle-down"></i></div>
+        </div>
+        <div id="content">
+            <div class="left-content">
+                <div class="container active" id="changchu">
+                    <i class="ti-home"></i>
+                    Trang chủ Admin
+                </div>
+
+                <div class="container">
+                    <div class="item-container" id="qlsanpham">
+                        Quản lý sản phẩm <i class="ti-angle-down"></i>
+                    </div>
+                    <ul class="menudrop-qlsanpham">
+                        <li class="item-menu"><i class="ti-plus"></i> Thêm sản phẩm</li>
+                        <li class="item-menu"><i class="ti-plus"></i> Sửa sản phẩm</li>
+                        <li class="item-menu"><i class="ti-plus"></i> Xóa sản phẩm</li>
+                    </ul>
+
+                </div>
+
+                <div class="container">
+                    <div class="item-container" id="donhang">
+                        Quản lý Bán Hàng <i class="ti-angle-down"></i>
+                    </div>
+                    <ul class="menudrop-donhang">
+                        <li class="item-menu"><i class="ti-plus"></i> Đơn hàng</li>
+                        <li class="item-menu"><i class="ti-plus"></i> Xem thống kê bán hàng</li>
+                    </ul>
+                </div>
+                <div class="container">
+                    <div class="item-container" id="thongke">
+                        Thống kê doanh thu<i class="ti-angle-down"></i>
+                    </div>
+                    <ul class="menudrop-">
+                        <!-- Thích thì thêm item cho dropmenu bằng cách đặt tên class rồi qua admin.css thêm thuộc tính-->
+                    </ul>
+                </div>
+                <div class="container">
+                    <div class="item-container" id="qlquyen">
+                        Quản lý quyền <i class="ti-angle-down"></i>
+                    </div>
+                    <ul class="menudrop-">
+                        <!-- Thích thì thêm item cho dropmenu bằng cách đặt tên class rồi qua admin.css thêm thuộc tính-->
+                    </ul>
+                </div>
+                <div class="container">
+                    <div class="item-container" id="taikhoan">
+                        Quản lý tài khoản <i class="ti-angle-down"></i>
+                    </div>
+                    <ul class="menudrop-qltk">
+                        <li class="item-menu"><i class="ti-plus"></i> Thêm tài khoản</li>
+                        <li class="item-menu"><i class="ti-plus"></i> Sửa tài khoản</li>
+                        <li class="item-menu"><i class="ti-plus"></i> Xóa tài khoản</li>
+                    </ul>
+                </div>
+            </div>
+            <div id="right-content">
+                <!-- Cần làm ở đây -->
+                <!-- Có thể dùng model có top-menu bên dưới -->
+                <!-- <div class="model-tk model-right">
+                    <div class="top-menu">
+                    <ul class="list-group list-group-horizontal menu-container">
+                        <li class="list-group-item model-item">Thêm tài khoản</li>
+                        <li class="list-group-item model-item">Sửa tài khoản</li>
+                        <li class="list-group-item model-item">Xóa tài khoản</li>
+                    </ul>
+                    </div>
+                    <div class="model-content">
+                    </div>
+                </div> -->
+
+            </div>
+        </div>
+    </div>
+    <?php 
+        include './pages/front/footer.php';
+    ?>
+    <script src="./JS/vadidation.js"></script>
+    <script src="./JS/jquery-3.4.1.min.js"></script>
+    <script src="./JS/admin.js"></script>
+</body>
+</html>
